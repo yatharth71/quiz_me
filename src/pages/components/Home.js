9c7a1fbe93a9fdf1f68c535bcae5eeb34aee0c9e
Yatharth Singh <yatharths863@gmail.com>
2021-12-05 10:42:04 +0530

    ADD: CONTENT

--- src/pages/components/Home.js
+++ src/pages/components/Home.js
@@ -1,7 +1,20 @@
 import React from "react";
 import styled from "styled-components";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 function Home() {
+
+  let settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true
+  }
+
   return (
     <Container>
       <Navbar>
@@ -14,6 +27,19 @@ function Home() {
       </Navbar>
       <Content>
         <Heading>The 100% FUN PLATFORM FOR LEARNERS</Heading>
+        <CreateBtn>
+          <h1>Create a QUIZ</h1>
+          <button>CREATE</button>
+        </CreateBtn>
+        <JoinBtn>
+          <h1>Join a QUIZ</h1>
+          <button>JOIN</button>
+        </JoinBtn>
+        <Carousel {...settings}>
+          <WrapCarousel>
+            <img src="https://media.istockphoto.com/photos/and-a-concept-yellow-question-mark-glowing-amid-black-question-marks-picture-id1305169776?b=1&k=20&m=1305169776&s=170667a&w=0&h=mpYdh2MzGN_rqxoRNlO5KWGCCq3ZktzSfp-wA0nD9no=" alt="First Image of the Carousel" />
+          </WrapCarousel>
+        </Carousel>
       </Content>
     </Container>
   );
@@ -24,6 +50,10 @@ export default Home;
 const Container = styled.main`
   @import url("https://fonts.googleapis.com/css2?family=Oswald&display=swap");
   @import url("https://fonts.googleapis.com/css2?family=Quicksand:wght@500&display=swap");
+  @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@700&display=swap');
+  * {
+    box-sizing: border-box;
+  }
 `
 
 const Navbar = styled.div`
@@ -64,4 +94,64 @@ const Heading = styled.h1`
   font-family: "Quicksand", sans-serif;
   color: #4f2525;
   margin-top: 50px;
+`
+
+const CreateBtn = styled.div`
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  margin-top: 10px;
+  font-family: 'Roboto', sans-serif;
+
+  button {
+    width: 10vw;
+    height: 5vh;
+    margin-top: 20px;
+    margin-inline: 30px;
+    cursor: pointer;
+    background: #192133;
+    color: white;
+    border-radius: 10px;
+    border: none;    
+    font-family: "Quicksand", sans-serif;
+    font-weight: bold;
+    
+    &:hover{
+      
+    }
+  }
+`
+
+const JoinBtn = styled.div`
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  margin-top: -10px;
+  font-family: 'Roboto', sans-serif;
+
+  button {
+    width: 10vw;
+    height: 5vh;
+    margin-top: 20px;
+    margin-inline: 30px;
+    cursor: pointer;
+    background: #192133;
+    color: white;
+    border-radius: 10px;
+    border: none;    
+    font-family: "Quicksand", sans-serif;
+    font-weight: bold;
+    
+    &:hover{
+      
+    }
+  }
+`
+
+const Carousel = styled(Slider) `
+
+`
+
+const WrapCarousel = styled.div `
+
 `
